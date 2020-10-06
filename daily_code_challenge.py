fae83cab775e44d7ecb92d62539fa2c1ac69fb7d
Srikanth <Srikanth.Karingula@yahoo.com>
2020-10-06 23:49:30 +0530

    Daily coding challenge updated new question

--- daily_code_challenge.py
+++ daily_code_challenge.py
@@ -59,10 +59,24 @@ print(solve_OneIteration(arr, sum_k))
 # Implement car and cdr.
 
 
-def cons(a, b):
-    def pair(f):
-        return f(a, b)
-    return pair
+# def cons(a, b):
+#     def pair(f):
+#         return f(a, b)
+#     return pair
 
 
-print(cons(3, 4))
+# print(cons(3, 4))
+
+
+# This problem was asked by Facebook.
+
+# Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count the number of ways it can be decoded.
+
+# For example, the message '111' would give 3, since it could be decoded as 'aaa', 'ka', and 'ak'.
+
+# You can assume that the messages are decodable. For example, '001' is not allowed.
+
+
+letters = {'a': 1, 'b': 2}
+
+print(letters['a'])
